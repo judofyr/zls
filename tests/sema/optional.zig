1e5ef9499a352ea17526c147bfb976eb4167ea44
Techatrix <19954306+Techatrix@users.noreply.github.com>
2023-08-22 22:57:46 +0200

    add TODO test for equality comparison between optionals

--- tests/sema/optional.zig
+++ tests/sema/optional.zig
@@ -29,3 +29,14 @@ const OptValNeqNull = @as(?u32, 3) != null;
 //    ^^^^^^^^^^^^^ (bool)(true)
 const NullNeqOptVal = null != @as(?u32, 3);
 //    ^^^^^^^^^^^^^ (bool)(true)
+
+// TODO
+// const Opt3EqOpt3 = @as(?u32, 3) == @as(?u32, 3);
+// //    ^^^^^^^^^^ (bool)(true)
+// const Opt2EqOpt3 = @as(?u32, 2) == @as(?u32, 3);
+// //    ^^^^^^^^^^ (bool)(false)
+//
+// const Opt3NeqOpt3 = @as(?u32, 3) != @as(?u32, 3);
+// //    ^^^^^^^^^^ (bool)(false)
+// const Opt2NeqOpt3 = @as(?u32, 2) != @as(?u32, 3);
+// //    ^^^^^^^^^^ (bool)(true)
