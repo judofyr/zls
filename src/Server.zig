2ebb24bf91d908610fdf403cf8ea2288f4dee523
Techatrix <19954306+Techatrix@users.noreply.github.com>
2023-09-09 20:23:57 +0200

    fix memory errors related to astgen_analyser

--- src/Server.zig
+++ src/Server.zig
@@ -886,8 +886,9 @@ pub fn updateConfiguration(server: *Server, new_config: configuration.Configurat
         }
         server.document_store.cimports.clearAndFree(server.document_store.allocator);
     }
-    
-    if (server.config.analysis_backend == .astgen_analyser) {
+
+    if (server.config.analysis_backend == .astgen_analyser) blk: {
+        if (server.mod != null) break :blk;
         server.mod = Module.init(server.allocator, &server.ip, &server.document_store);
         server.document_store.mod = &server.mod.?;
     } else if (server.mod) |*mod| {
@@ -910,7 +911,6 @@ pub fn updateConfiguration(server: *Server, new_config: configuration.Configurat
         );
         server.allocator.free(json_message);
     }
-
 
     // <---------------------------------------------------------->
     //  don't modify config options after here, only show messages
